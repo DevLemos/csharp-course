84f93c0b301dd5ededa2c2213b735f5bbc111325
DevLemos <kauancode@outlook.com>
2024-09-05 14:49:25 -0300

    implicit conversion and casting

---
+++ Introducao/conversao-implicita-e-casting/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace conversao_implicita_e_casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //exemplo 1
+            double a;
+            float b;
+            a = 5.1;
+            b = (float)a;
+            Console.WriteLine(b);
+
+            //exemplo 2
+            double l;
+            int c;
+            l = 5.1;
+            c = (int)l;
+            Console.WriteLine(c);
+
+            //exemplo 3
+            int j = 5;
+            int k = 2;
+            double resultado = (double)j / k;
+            Console.WriteLine(resultado);
+        }
+    }
+}
