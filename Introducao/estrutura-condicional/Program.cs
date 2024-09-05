e009fa6517bb037501382c9f77d23fe991caaa9c
DevLemos <kauancode@outlook.com>
2024-09-05 15:32:15 -0300

    conditional structure

---
+++ Introducao/estrutura-condicional/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace estrutura_condicional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //exemplo 1
+            Console.WriteLine("Entre com um número inteiro:");
+            int x = int.Parse(Console.ReadLine());
+            if (x % 2 == 0)
+            {
+                Console.WriteLine("Par!");
+            }
+            else
+            {
+                Console.WriteLine("Impar");
+            }
+
+            //exemplo 2
+            Console.WriteLine("Qual a hora atual?");
+            int hora = int.Parse(Console.ReadLine());
+            if (hora < 12)
+            {
+                Console.WriteLine("Bom dia!");
+            }
+            else if (hora < 18)
+            {
+                Console.WriteLine("Boa tarde!");
+            }
+            else
+            {
+                Console.WriteLine("Boa noite!");
+            }
+        }
+    }
+}
