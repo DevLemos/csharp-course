043003fc055da909041c32ce78a2a98b0818aa44
DevLemos <kauancode@outlook.com>
2024-09-12 15:32:45 -0300

    Encapsulamento

---
+++ POO/encapsulamento/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+using encapsulamento.entities;
+
+namespace encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("TV", 500.67, 2);
+            Console.WriteLine(p.GetNome());
+            p.SetNome("TV 4K");
+            Console.WriteLine(p.GetNome());
+        }
+    }
+}
